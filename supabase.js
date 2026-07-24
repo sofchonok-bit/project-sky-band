f3191f81f4fe76b7170c6d4b0be5cf220ec517ff
sofchonok-bit <sofchonok@gmail.com>
2026-07-24 14:19:42 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://fvyosrciypwihzsflvtp.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable__m0fKqKJmIll4ajGAo16jw_-iUkrqCM";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
